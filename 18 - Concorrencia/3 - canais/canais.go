f6f98e6a1ecad5e7fd0da6bc1ddfe2616d7e22fd
Rodrigo Aparecido Fernandes Simao <rafs1991@gmail.com>
2021-06-25 11:21:47 -0300

    Estudando...

---
+++ 18 - Concorrencia/3 - canais/canais.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	canal := make(chan string)
+
+	go escrever("Olá mundo", canal)
+
+	for mensagem := range canal {
+		fmt.Println(mensagem)
+	}
+
+	fmt.Println("Fim do programa!")
+}
+
+func escrever(texto string, canal chan string) {
+	for i := 0; i < 5; i++ {
+		canal <- texto
+		time.Sleep(time.Second)
+	}
+
+	close(canal)
+}
