59bc86b407277e626c08b51b60cc04b67e46dd47
Rodrigo Aparecido Fernandes Simao <rafs1991@gmail.com>
2021-06-30 10:38:00 -0300

    Estudando...

---
+++ 18 - Concorrencia/6 - Padrões de Concorrência/6.3 - Multiplexador/multiplexador.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	canal := multiplexar(escrever("Olá mundo"), escrever("mundo doido"))
+
+	for i := 0; i < 10; i++ {
+		fmt.Println(<-canal)
+	}
+}
+
+func multiplexar(canalDeEntrada1, canalDeEntrada2 <-chan string) <-chan string {
+	canalDeSaida := make(chan string)
+
+	go func() {
+		for {
+			select {
+			case mensagem := <-canalDeEntrada1:
+				canalDeSaida <- mensagem
+			case mensagem := <-canalDeEntrada2:
+				canalDeSaida <- mensagem
+			}
+		}
+	}()
+
+	return canalDeSaida
+}
+
+func escrever(texto string) <-chan string {
+	canal := make(chan string)
+
+	go func() {
+		for {
+			canal <- fmt.Sprintf("Valor recebido : %s", texto)
+			time.Sleep(time.Millisecond * 500)
+		}
+	}()
+
+	return canal
+}
